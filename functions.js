1c3c6d12540efa480c07e81ef435bf055ff855aa
DevAndreAkira <pianista.akira@gmail.com>
2022-04-11 14:25:33 -0300

    Updates

--- functions.js
+++ functions.js
@@ -1,81 +1,85 @@
-function trocandoLogo() {
-    let tela = document.querySelector("#root").offsetWidth;
-    if (tela > 991) {
-        document.querySelector("#navbarSupportedContent > ul > li:nth-child(1) > a").classList.remove("nav-link")
+window.location.href.onchange = function () {
+
+    function trocandoLogo() {
+        let tela = document.querySelector("#root").offsetWidth;
+        if (tela > 991) {
+            document.querySelector("#navbarSupportedContent > ul > li:nth-child(1) > a").classList.remove("nav-link")
+        }
+        else {
+            document.querySelector("#navbarSupportedContent > ul > li:nth-child(1) > a").classList.add("nav-link")
+        }
     }
-    else {
-        document.querySelector("#navbarSupportedContent > ul > li:nth-child(1) > a").classList.add("nav-link")
+
+    function ativandoMenu(numero) {
+        let endereco = window.location.href;
+
+        endereco = endereco.split("/");
+        console.log(endereco);
+        endereco.forEach((e, index) => {
+            if (endereco[index] === "sobre") {
+                numero = 2;
+            }
+            else if (endereco[index] === "historico") {
+                numero = 2;
+            }
+            else if (endereco[index] === "habilidades") {
+                numero = 3;
+            }
+            else if (endereco[index] === "portfolio") {
+                numero = 4;
+            }
+            else if (endereco[index] === "hobbies") {
+                numero = 5;
+            }
+            else if (endereco[index] === "contato") {
+                numero = 6;
+            }
+        })
+        document.querySelector(`#navbarSupportedContent > ul > li:nth-child(${numero}) > a`).classList.add("active");
     }
-}
 
-function ativandoMenu(numero) {
-    let endereco = window.location.href;
+    window.onload = function () {
+        trocandoLogo();
+        ativandoMenu();
 
-    endereco = endereco.split("/");
-    console.log(endereco);
-    endereco.forEach((e, index) => {
-        if (endereco[index] === "sobre") {
-            numero = 2;
-        }
-        else if (endereco[index] === "historico") {
-            numero = 2;
-        }
-        else if (endereco[index] === "habilidades") {
-            numero = 3;
-        }
-        else if (endereco[index] === "portfolio") {
-            numero = 4;
-        }
-        else if (endereco[index] === "hobbies") {
-            numero = 5;
-        }
-        else if (endereco[index] === "contato") {
-            numero = 6;
-        }
-    })
-    document.querySelector(`#navbarSupportedContent > ul > li:nth-child(${numero}) > a`).classList.add("active");
-}
+        let array = document.querySelectorAll(".hobbies div > div:nth-child(n) > div > a > img");
+        array.forEach((e, index) => {
+            const heightOutput = document.querySelector(".hobbies #uncontrolled-tab-example-tabpane-home > div > div:nth-child(1) > div > a > img").offsetHeight;
+            const widthOutput = document.querySelector(".hobbies #uncontrolled-tab-example-tabpane-home > div > div:nth-child(1) > div > a > img").offsetWidth;
+            array[index].style.minHeight = `${heightOutput}px`;
+            array[index].style.width = `${widthOutput}px`;
+        })
 
-window.onload = function () {
-    trocandoLogo();
-    ativandoMenu();
+        // TENTANDO ARRUMAR A LARGURA
+        let arrayBtn = document.querySelectorAll("main .nav-item button");
+        arrayBtn.forEach((e, index) => {
+            arrayBtn[index].onclick = function () {
+                responsiveW();
+            }
+        })
+    }
 
-    let array = document.querySelectorAll(".hobbies div > div:nth-child(n) > div > a > img");
-    array.forEach((e, index) => {
-        const heightOutput = document.querySelector(".hobbies #uncontrolled-tab-example-tabpane-home > div > div:nth-child(1) > div > a > img").offsetHeight;
-        const widthOutput = document.querySelector(".hobbies #uncontrolled-tab-example-tabpane-home > div > div:nth-child(1) > div > a > img").offsetWidth;
-        array[index].style.minHeight = `${heightOutput}px`;
-        array[index].style.width = `${widthOutput}px`;
-    })
+    function responsiveW() {
+        let imgs = document.querySelectorAll(".hobbies img");
+        imgs.forEach((e, index) => {
+            // WIDTH
+            // console.log(document.querySelectorAll(".card")[index].offsetWidth);
+            imgs[index].style.width = `${(document.querySelectorAll(".card")[index].offsetWidth - 20)}px`;
 
-    // TENTANDO ARRUMAR A LARGURA
-    let arrayBtn = document.querySelectorAll("main .nav-item button");
-    arrayBtn.forEach((e, index) => {
-        arrayBtn[index].onclick = function () {
-            responsiveW();
-        }
-    })
-}
+            // ARRUMANDO A ALTURA - DEPOIS
+            // let altura = document.querySelector("#uncontrolled-tab-example-tabpane-home > div > div > div > a > img").offsetHeight;
+            // imgs[index].style.height = altura;
+        })
+    }
 
-function responsiveW() {
-    let imgs = document.querySelectorAll(".hobbies img");
-    imgs.forEach((e, index) => {
-        // WIDTH
-        // console.log(document.querySelectorAll(".card")[index].offsetWidth);
-        imgs[index].style.width = `${(document.querySelectorAll(".card")[index].offsetWidth - 20)}px`;
-
-        // ARRUMANDO A ALTURA - DEPOIS
-        // let altura = document.querySelector("#uncontrolled-tab-example-tabpane-home > div > div > div > a > img").offsetHeight;
-        // imgs[index].style.height = altura;
-    })
-}
-
-window.onresize = function () {
-    responsiveW();
-}
+    window.onresize = function () {
+        responsiveW();
+    }
 
 
-window.onresize = function () {
-    trocandoLogo();
-    responsiveW();
+    window.onresize = function () {
+        trocandoLogo();
+        responsiveW();
+    }
+
 }
