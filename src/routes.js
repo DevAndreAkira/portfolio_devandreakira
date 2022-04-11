78d9996b76c3660235b515f51b1d2c9c70158443
DevAndreAkira <pianista.akira@gmail.com>
2022-04-11 19:24:07 -0300

    Apenas um teste

--- src/routes.js
+++ src/routes.js
@@ -13,13 +13,14 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/portfolio_devandreakira/" exact element={<Home />} />
-                <Route path="/portfolio_devandreakira/sobre" element={<Sobre />} />
-                <Route path="/portfolio_devandreakira/historico" element={<Histórico />} />
-                <Route path="/portfolio_devandreakira/habilidades" element={<Habilidades />} />
-                <Route path="/portfolio_devandreakira/portfolio" element={<Portfolio />} />
-                <Route path="/portfolio_devandreakira/hobbies" element={<Hobbies />} />
-                <Route path="/portfolio_devandreakira/contato" element={<Contato />} />
+                <Route path="/" exact element={<Home />} />
+                <Route path="/sobre" element={<Sobre />} />
+                <Route path="/historico" element={<Histórico />} />
+                <Route path="/habilidades" element={<Habilidades />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/hobbies" element={<Hobbies />} />
+                <Route path="/contato" element={<Contato />} />
+                <Route path='*' element={<h1 style={{color:"#05c3c3"}}>Not Found 404</h1>} />
             </Routes>
         </BrowserRouter>
     );
