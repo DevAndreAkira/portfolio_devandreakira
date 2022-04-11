7d4abab556fe64def10b2131f7cccea808d485f0
DevAndreAkira <pianista.akira@gmail.com>
2022-04-11 19:34:11 -0300

    Voltando configs

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -111,28 +111,28 @@ const Header = () => {
                         <div className="collapse navbar-collapse flex-column" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0 flex-column">
                                 <li className="nav-item">
-                                    <Link className="navbar-brand nav-link" to="/"><img src={logo} alt="Dev André Akira"
+                                    <Link className="navbar-brand nav-link" to="/portfolio_devandreakira"><img src={logo} alt="Dev André Akira"
                                         width="120" className="logo" />
                                         <p className='mb-4'>{i18n.t('ola.ola')}<br />André Akira</p>
                                     </Link>
                                 </li>
                                 <li className="nav-item d-flex align-items-center">
-                                    <Link className="nav-link" aria-current="page" to="/sobre"><FaInfoCircle className='icons' />{i18n.t('nav.sobre')}</Link>
+                                    <Link className="nav-link" aria-current="page" to="/portfolio_devandreakira/sobre"><FaInfoCircle className='icons' />{i18n.t('nav.sobre')}</Link>
                                 </li>
                                 <li className="nav-item d-flex align-items-center">
-                                    <Link className="nav-link" to="/historico"><BiHistory className='icons' />{i18n.t('nav.historico')}</Link>
+                                    <Link className="nav-link" to="/portfolio_devandreakira/historico"><BiHistory className='icons' />{i18n.t('nav.historico')}</Link>
                                 </li>
                                 <li className="nav-item d-flex align-items-center">
-                                    <Link className="nav-link" to="/habilidades"><GiSkills className='icons' />{i18n.t('nav.habilidades')}</Link>
+                                    <Link className="nav-link" to="/portfolio_devandreakira/habilidades"><GiSkills className='icons' />{i18n.t('nav.habilidades')}</Link>
                                 </li>
                                 <li className="nav-item d-flex align-items-center">
-                                    <Link className="nav-link" to="/portfolio"><BiBookHeart className='icons' />{i18n.t('nav.portfolio')}</Link>
+                                    <Link className="nav-link" to="/portfolio_devandreakira/portfolio"><BiBookHeart className='icons' />{i18n.t('nav.portfolio')}</Link>
                                 </li>
                                 <li className="nav-item d-flex align-items-center">
-                                    <Link className="nav-link" to="/hobbies"><GiMusicalNotes className='icons' />{i18n.t('nav.hobbies')}</Link>
+                                    <Link className="nav-link" to="/portfolio_devandreakira/hobbies"><GiMusicalNotes className='icons' />{i18n.t('nav.hobbies')}</Link>
                                 </li>
                                 <li className="nav-item d-flex align-items-center">
-                                    <Link className="nav-link" to="/contato"><SiMinutemailer className='icons' />{i18n.t('nav.contato')}</Link>
+                                    <Link className="nav-link" to="/portfolio_devandreakira/contato"><SiMinutemailer className='icons' />{i18n.t('nav.contato')}</Link>
                                 </li>
                             </ul>
                         </div>
