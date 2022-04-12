2ff0cf85adaf4c71aafae2e97b04902741237c79
DevAndreAkira <pianista.akira@gmail.com>
2022-04-12 09:38:04 -0300

    404 github page spa

--- src/routes.js
+++ src/routes.js
@@ -12,15 +12,15 @@ import Contato from './Pages/Contato';
 const Router = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/portfolio_devandreakira/" exact element={<Home />} />
-                <Route path="/portfolio_devandreakira/sobre" element={<Sobre />} />
-                <Route path="/portfolio_devandreakira/historico" element={<Histórico />} />
-                <Route path="/portfolio_devandreakira/habilidades" element={<Habilidades />} />
-                <Route path="/portfolio_devandreakira/portfolio" element={<Portfolio />} />
-                <Route path="/portfolio_devandreakira/hobbies" element={<Hobbies />} />
-                <Route path="/portfolio_devandreakira/contato" element={<Contato />} />
-                <Route path='*' element={<h1 style={{color:"#05c3c3"}}>Not Found 404</h1>} />
+            <Routes basename="/portfolio_devandreakira">
+                <Route path='*' element={<h1 style={{ color: "#05c3c3" }}>Not Found 404</h1>} />
+                <Route path="/portfolio_devandreakira/" element={<Home />} />
+                <Route path="/portfolio_devandreakira/sobre" exact element={<Sobre />} />
+                <Route path="/portfolio_devandreakira/historico" exact element={<Histórico />} />
+                <Route path="/portfolio_devandreakira/habilidades" exact element={<Habilidades />} />
+                <Route path="/portfolio_devandreakira/portfolio" exact element={<Portfolio />} />
+                <Route path="/portfolio_devandreakira/hobbies" exact element={<Hobbies />} />
+                <Route path="/portfolio_devandreakira/contato" exact element={<Contato />} />
             </Routes>
         </BrowserRouter>
     );
